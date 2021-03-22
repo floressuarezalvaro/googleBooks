6c8c4f7364a0739ecc690671b300e7051fe60f14
Alvy <floressuarezalvaro@gmail.com>
2021-03-21 20:01:41 -0700

    first commit

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 5000;
+const path = require("path");
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
+
+app.use("/api", require("./routes/apiRoutes"));
+app.use("/books", require("./routes/bookRoutes"));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+});
+
+app.listen(PORT, () => console.log(`http://localhost:${PORT}`));
