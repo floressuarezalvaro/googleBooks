2e4ec6b403696284e084db98520ece68300b9c8b
Alvy <floressuarezalvaro@gmail.com>
2021-03-25 19:15:13 -0700

    update api routes on front end

---
+++ client/src/utils/API.js
@@ -0,0 +1,13 @@
+import axios from "axios";
+
+export default {
+    getBooks: function() {
+        return axios.get("/book");
+    },
+    saveBook: function(bookResults) {
+        return axios.post("/book", bookResults);
+    },
+    deleteBook: function(id) {
+        return axios.delete("/book/" + id);
+    },
+};
