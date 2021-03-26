2e1610f60a256462a536e44e5d398c6d19dfaf46
Alvy <floressuarezalvaro@gmail.com>
2021-03-25 20:07:38 -0700

    updated server strucutre for heroku devployement

--- server.js
+++ server.js
@@ -18,13 +18,12 @@ mongoose
   .then(() => console.log("MongoDB is connected"))
   .catch((err) => console.log(err));
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-}
 app.use("/book", require("./routes/bookRoute"));
 
-app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-});
+if (process.env.NODE_ENV === "production") {
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 app.listen(PORT, () => console.log(`http://localhost:${PORT}`));
